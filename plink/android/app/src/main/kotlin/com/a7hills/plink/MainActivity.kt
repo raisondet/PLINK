8432f28b2e66cca8d14abba74472802ec738bdfa
cch <raisondet@gmail.com>
2021-12-14 06:26:58 +0900

    make splash page

---
+++ plink/android/app/src/main/kotlin/com/a7hills/plink/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.a7hills.plink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ plink/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:plink/routes.dart';
+import 'package:plink/theme.dart';
+import 'package:plink/screens/splash/splash_screen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: theme(),
+      // home: SplashScreen(),
+      // We use routeName so that we dont need to remember the name
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
